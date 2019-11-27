236c2ada5fc3bb00d23fa7ca58f72370d29dbd54
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-11-26 17:23:09 -0800

    remove unnecessary comment

--- main.go
+++ main.go
@@ -6,19 +6,6 @@ import (
 	"github.com/sourcegraph/terraform-provider-site24x7/site24x7"
 )
 
-// main is a plugin main, not a "real" main.
-// Please set the value of environment variable `SITE24X7_AUTHTOKEN_FILE` to a path to a JSDN file
-// This JSDN file stores the client id, client secret and refresh token necessary to obtain OAuth2 access tokens from
-// site24x7. Expected is the following content for the JSON file:
-//
-// {
-//    "CLIENT_ID": "xxxx_your_client_id_xxxxx",
-//    "CLIENT_SECRET": "xxxx_your_client_secret_xxxxx",
-//    "REFRESH_TOKEN": "xxxx_your_refresh_token_xxxxx",
-// }
-//
-// If `SITE24X7_AUTHTOKEN_FILE` is not set it will use `site24x7-oauth.json` in the current working directory.
-// A helper command in site24x7/oauth/cmd/site24x7-oauth can be used to generate the JSON file.
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
